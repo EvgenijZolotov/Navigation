70ea8e1e51c09302c5ca274ee74ca2ba8a780883
Evgenij Zolotov <evgenij.zolotov@mail.ru>
2022-09-27 19:05:29 +0300

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Evgenij Zolotov on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
